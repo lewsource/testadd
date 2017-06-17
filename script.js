aa305f5e8db2e9408e7c25ab0d73248199068456
lewispayne <lewiswpayne@gmail.com>
2017-06-17 15:10:31 +0100

    originalCode

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(10000);
+  
+  $('.content').hide();
+  
+  $('.openingbit').on('click', function() {
+		$('.content').show();
+	});
+}
+
+$(document).ready(main);
